1e5a479c2279826336d2b3ff7b11658f6bfd0cb7
jin <winterchocolatte@gmail.com>
2019-08-04 15:43:12 -0400

    creaetd __init__.pys

--- utils/emotion_analyzer.py
+++ utils/emotion_analyzer.py
@@ -1,4 +1,8 @@
 from ibm_watson import ToneAnalyzerV3
 
 tone_analyzer = ToneAnalyzerV3(
+    version='2019-02-28',
+    iam_apikey='ank5dcbV5ewqQ1_eOyrw2eTW0nE13i0AWhTjgcE7NnfD',
+    url='https://gateway.watsonplatform.net/tone-analyzer/api'
 )
+
