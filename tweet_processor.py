1dc097211e1a609bd9f9e2fb4e0c183dcb4abbbf
rjhidalgo <36284939+rjhidalgo@users.noreply.github.com>
2019-08-16 15:04:17 -0400

    testing replies
    testing

--- tweet_processor.py
+++ tweet_processor.py
@@ -1,7 +1,10 @@
 import tweepy
 import time
+import random
 
 from keys.keys import *
+from scraper import *
+
 
 
 def get_all_tweets_follower():
@@ -49,3 +52,37 @@ def get_follower_ids():
         time.sleep(1)
     
     return ids
+
+def reply_to_tweet(user_id, tweet_id):
+    
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+    api = tweepy.API(auth)
+
+    run()
+    imageLocs = []
+    count = 0
+
+    username = api.get_user(user_id).screen_name
+    
+    try:
+        fp = open("images.txt", 'r')
+        
+        line = fp.readline
+        imageLocs.append(line)
+
+        while line:
+            line = fp.readline
+            imageLocs.append(line)
+
+    finally:
+        fp.close()
+    
+    size = len(imageLocs)
+    choose = random.randint(0, size - 1)
+    reply = imageLocs[choose]    
+
+    api.update_status("@<"+ username + "> " + reply, tweet_id)
+    
+
+reply_to_tweet(1132848667092803584, 1133213334302527494)
